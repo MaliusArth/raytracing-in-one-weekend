fb03c06a9d5ace658131dcea1659cd7e9ce1c98d
MaliusArth <dev.malius@chello.at>
2024-11-24 04:08:49 +0100

    clean up batch script comments

--- build.bat
+++ build.bat
@@ -1,5 +1,9 @@
 @echo off
 
+:: batch comments ref
+:: https://stackoverflow.com/questions/12407800/which-comment-style-should-i-use-in-batch-files
+:: https://stackoverflow.com/questions/64186507/question-about-comments-in-batch-bat-files-and-speed/68436626#68436626
+
 echo %~0 %*
 
 setlocal EnableDelayedExpansion
@@ -11,18 +15,18 @@ setlocal EnableDelayedExpansion
 
 :: defaults
 
-@REM set "-test=0"
+:: set "-test=0"
 set "source=%~1"
-@REM if not exist %source% echo source '%source%' does not exist! && goto:eof
+:: if not exist %source% echo source '%source%' does not exist! && goto:eof
 shift
 
 :parseArgs
 call:getArgFlag "-file" "-file" "%~1" && shift && goto :parseArgs
 call:getArgFlag "-test" "-test" "%~1" && shift && goto :parseArgs
-@REM call:getArgWithValue "-file" "-file" "%~1" "%~2" && shift && shift && goto :parseArgs
+:: call:getArgWithValue "-file" "-file" "%~1" "%~2" && shift && shift && goto :parseArgs
 
-@REM echo -file: %-file%
-@REM echo -test: %-test%
+:: echo -file: %-file%
+:: echo -test: %-test%
 
 if "%-file%" == "1" (
     for %%A in ("%source%\.") do set "out_name=%%~nA"
@@ -34,67 +38,72 @@ if "%-file%" == "1" (
 set "command=build"
 if "%-test%" == "1" set "command=test"
 
-@REM set "project_path=%~dp0"
-@REM for %%A in ("%~p0\.") do set "project_name=%%~nxA"
+:: set "project_path=%~dp0"
+:: for %%A in ("%~p0\.") do set "project_name=%%~nxA"
 
 set "out_dir=bin"
 
-: -vet
-:   -vet-unused
-:   -vet-unused-variables
-:   -vet-unused-imports
-:   -vet-shadowing
-:   -vet-using-stmt
+:: -vet
+::   -vet-unused
+::   -vet-unused-variables
+::   -vet-unused-imports
+::   -vet-shadowing
+::   -vet-using-stmt
 
-: TODO: use in git push hook to prevent 'using' getting commited to the repo
-: -vet-using-stmt & -vet-using-param
-:   'using' is considered bad practice outside of immediate refactoring.
+:: TODO: use in git push hook to prevent 'using' getting committed to the repo
+:: -vet-using-stmt & -vet-using-param
+::   'using' is considered bad practice outside of immediate refactoring.
 
-: -vet-style
-: -vet-semicolon
-: -vet-cast
-: -vet-tabs
+:: -vet-style
+:: -vet-semicolon
+:: -vet-cast
+:: -vet-tabs
 
-: -strict-style
-:   like -vet-style -vet-semicolon
-:   + Errs when the attached-brace style in not adhered to (also known as 1TBS).
-:   + Errs when 'case' labels are not in the same column as the associated 'switch' token.
-: -vet-style -vet-semicolon
+:: -strict-style
+::   like -vet-style -vet-semicolon
+::   + Errs when the attached-brace style in not adhered to (also known as 1TBS).
+::   + Errs when 'case' labels are not in the same column as the associated 'switch' token.
+:: -vet-style -vet-semicolon
 set STYLE_PARAMS=-vet -strict-style -vet-using-param -vet-cast -vet-tabs
 
-: TODO: -sanitize:address
+:: TODO: -sanitize:address
 
-: -vet
-@REM odin build src/%1.odin -debug -show-timings -out=bin/%1.exe -extra-linker-flags="/libpath:lib"
+:: -vet
+:: odin build src/%1.odin -debug -show-timings -out=bin/%1.exe -extra-linker-flags="/libpath:lib"
 
-@REM copy "lib\Debug\raylib_static.pdb" "bin\" >nul 2>&1
-@REM odin build src/%1.odin -debug -show-timings -out=bin/%1.exe -extra-linker-flags="/libpath:lib\Debug /NODEFAULTLIB:raylib_static" -subsystem:windows -keep-temp-files
+:: copy "lib\Debug\raylib_static.pdb" "bin\" >nul 2>&1
+:: odin build src/%1.odin -debug -show-timings -out=bin/%1.exe -extra-linker-flags="/libpath:lib\Debug /NODEFAULTLIB:raylib_static" -subsystem:windows -keep-temp-files
 
-@REM SET "raylib_dir=lib\raylib"
-@REM copy "%raylib_dir%\raylib.pdb" "bin\" >nul 2>&1
-@REM copy "%raylib_dir%\raylib.dll" "bin\" >nul 2>&1
-@REM  -keep-temp-files
-: -define:RAYLIB_SHARED=true to link against a dll version
-@REM -define:RAYLIB_SHARED=true
-@REM odin build src/%1.odin -file -out=bin/%1.exe -o:none -debug -show-timings -extra-linker-flags="/libpath:%raylib_dir%" -subsystem:console %STYLE_PARAMS%
-@REM odin strip-semicolon src/%file_name%.odin -file
+:: SET "raylib_dir=lib\raylib"
+:: copy "%raylib_dir%\raylib.pdb" "bin\" >nul 2>&1
+:: copy "%raylib_dir%\raylib.dll" "bin\" >nul 2>&1
+::  -keep-temp-files
+:: -define:RAYLIB_SHARED=true to link against a dll version
+:: -define:RAYLIB_SHARED=true
+:: odin build src/%1.odin -file -out=bin/%1.exe -o:none -debug -show-timings -extra-linker-flags="/libpath:%raylib_dir%" -subsystem:console %STYLE_PARAMS%
+:: odin strip-semicolon src/%file_name%.odin -file
 
 mkdir %out_dir% 2>nul
 
+:: -o:none
+:: -o:minimal (default)
+:: -o:size
+:: -o:speed
+:: -o:aggressive
 odin %command% %source% -out=%out_dir%/%out_name%.exe -o:speed -debug -show-timings -subsystem:console %STYLE_PARAMS%
 
-@REM start rundll32.exe cmdext.dll,MessageBeepStub
+:: start rundll32.exe cmdext.dll,MessageBeepStub
 start rundll32 user32.dll,MessageBeep
-@REM call powershell "[console]::beep(500,300)"
+:: call powershell "[console]::beep(500,300)"
 
-@REM || goto :error
-@REM echo -------------------------
+:: || goto :error
+:: echo -------------------------
 
-@REM goto :end
-@REM :error
-@REM if %errorlevel% neq 0 exit /b %errorlevel%
+:: goto :end
+:: :error
+:: if %errorlevel% neq 0 exit /b %errorlevel%
 
-@REM :end
+:: :end
 
 
 
@@ -110,13 +119,13 @@ goto:eof
 :: 3 argument name
 :: 4 argument value
 :: 5 default value
+::   if "%~4"=="" (
+::     REM unset the variable if value is not provided
+::     set "%~1="
+::     exit /B 0
+::   )
 :getArgWithValue
 if "%~3"=="%~2" (
-@REM   if "%~4"=="" (
-@REM     REM unset the variable if value is not provided
-@REM     set "%~1="
-@REM     exit /B 0
-@REM   )
   set "%~1=%~4"
   exit /B 0
 )
--- debug.bat
+++ debug.bat
@@ -1,9 +1,8 @@
 @echo off
-@REM TODO: update to properly support package folders
 
 echo %~0 %*
 
-@REM setlocal enabledelayedexpansion
+:: setlocal enabledelayedexpansion
 if "%1" == "" (
     echo "you forgot to pass a source file"
     goto :eof
@@ -12,58 +11,58 @@ if "%1" == "" (
 set "project_path=%~dp0"
 set "exe_name=%~n1"
 set "source_path=%~2"
-@REM set "source_name=%~n2"
+:: set "source_name=%~n2"
 if "%source_path%"=="" set "source_name=%exe_name%"
 
 
-@REM tasklist /fi "imagename eq raddbg.exe"
+:: tasklist /fi "imagename eq raddbg.exe"
 
 for /f %%i in ('qprocess "remedybg.exe" ^2^>nul ^| find /i /c "remedybg.exe"') do set "runningProcesses=%%i"
-@REM echo running debugger processes: %runningProcesses%
-@REM echo project path: %project_path%
+:: echo running debugger processes: %runningProcesses%
+:: echo project path: %project_path%
 if "%runningProcesses%"=="0" (
     start /max remedybg "bin\%exe_name%.exe"
 ) else (
     remedybg bring-debugger-to-foreground
 )
 
-@REM for %%A in (*.rdbg) do set session_file=%%~fsA
-@REM if exist "%session_file%" start remedybg open-session "%session_file%"
+:: for %%A in (*.rdbg) do set session_file=%%~fsA
+:: if exist "%session_file%" start remedybg open-session "%session_file%"
 
-@REM set /p input=press ENTER to continue...
-@REM start remedybg "bin\%exe_name%.exe"
-@REM start remedybg open-file "bin\%exe_name%.exe"
-@REM timeout /t 1
-@REM start /wait remedybg bring-debugger-to-foreground
+:: set /p input=press ENTER to continue...
+:: start remedybg "bin\%exe_name%.exe"
+:: start remedybg open-file "bin\%exe_name%.exe"
+:: timeout /t 1
+:: start /wait remedybg bring-debugger-to-foreground
 remedybg stop-debugging
 remedybg open-file "%source_path%"
 remedybg add-breakpoint-at-function "main.main"
 remedybg start-debugging
-@REM remedybg remove-breakpoint-at-function "main.main"
+:: remedybg remove-breakpoint-at-function "main.main"
 
 
 
-@REM TODO: else activate program window & exec ipc call (maybe the ipc call also activates it?)
+:: TODO: else activate program window & exec ipc call (maybe the ipc call also activates it?)
 
 exit /b
-@REM set
+:: set
 
 
 
 
-@REM @if (@a==@a) @end /*
-@REM     @echo off
-@REM     cscript //E:JScript //nologo "%~f0" %*
+:: @if (@a==@a) @end /*
+::     @echo off
+::     cscript //E:JScript //nologo "%~f0" %*
 
-@REM     exit /b %errorlevel%
-@REM */
+::     exit /b %errorlevel%
+:: */
 
-@REM // --- JScript code below this line ----
+:: // --- JScript code below this line ----
 
-@REM var WshShell = WScript.CreateObject("WScript.Shell");
-@REM // var ARGS = WScript.Arguments;
+:: var WshShell = WScript.CreateObject("WScript.Shell");
+:: // var ARGS = WScript.Arguments;
 
-@REM WshShell.AppActivate("The RAD Debugger");
-@REM // WshShell.SendKeys("{F5}");
-@REM // WScript.Echo(ARGS.Item(0) + " activated");
-@REM WScript.Quit(0);
+:: WshShell.AppActivate("The RAD Debugger");
+:: // WshShell.SendKeys("{F5}");
+:: // WScript.Echo(ARGS.Item(0) + " activated");
+:: WScript.Quit(0);
